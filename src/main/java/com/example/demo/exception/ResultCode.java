6d5ac1a061a4f597a6afde8b1cf9ef4811c80e9b
dmsdms39 <dmsdms39@cau.ac.kr>
2021-12-28 17:21:16 +0900

    Exception 처리 수정

--- src/main/java/com/example/demo/exception/ResultCode.java
+++ src/main/java/com/example/demo/exception/ResultCode.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ResultCode {
-    NAME_DUPLICATION(HttpStatus.INTERNAL_SERVER_ERROR,"NAME-DUPLICATED-ERR-404","동일한 이름이 존재합니다."),;
+    NAME_DUPLICATION(HttpStatus.INTERNAL_SERVER_ERROR,"NAME-DUPLICATED-ERR-400","동일한 이름이 존재합니다."),;
 
     private HttpStatus status;
     private String code;
