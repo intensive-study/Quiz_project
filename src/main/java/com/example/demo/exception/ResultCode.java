a68afbf4681c8a0773080b21c66cdd6aa170840f
dmsdms39 <dmsdms39@cau.ac.kr>
2022-01-06 02:51:06 +0900

    컨트롤러 테스트 추가, 퀴즈 서비스테스트 삭제, Exception 변경, data-h2 데이터 추가

--- src/main/java/com/example/demo/exception/ResultCode.java
+++ src/main/java/com/example/demo/exception/ResultCode.java
@@ -6,9 +6,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ResultCode {
-    NAME_DUPLICATION(HttpStatus.INTERNAL_SERVER_ERROR,"NAME-DUPLICATED-ERR-400","동일한 이름이 존재합니다."),
-    ID_NOT_EXIST(HttpStatus.INTERNAL_SERVER_ERROR,"ID_NOT_EXIST-ERR-400","해당 ID가 존재하지 않습니다."),;
-
+    NAME_DUPLICATION(HttpStatus.INTERNAL_SERVER_ERROR,"NAME-DUPLICATED-ERR-500","동일한 이름이 존재합니다."),
+    ID_NOT_EXIST(HttpStatus.INTERNAL_SERVER_ERROR,"ID-NOT-EXIST-ERR-500","해당 ID를 가진 데이터가 없습니다."),
+    NON_AUTHORITATIVE_INFORMATION(HttpStatus.NON_AUTHORITATIVE_INFORMATION, "NON-AUTHORITATIVE-ERR-400", "수정 / 삭제 권한이 없습니다."),
+    NOT_ACCEPTABLE(HttpStatus.NOT_ACCEPTABLE, "NOT-ACCEPTABLE-QUERY-ERR-400", "수행할 수 없습니다.(무결성 제약 조건 위반)");
     private HttpStatus status;
     private String code;
     private String message;
--- src/main/java/com/example/demo/vo/RequestQuiz.java
+++ src/main/java/com/example/demo/vo/RequestQuiz.java
@@ -1,5 +1,6 @@
 package com.example.demo.vo;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,11 +8,15 @@ import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
+@Builder
 public class RequestQuiz {
 
     private Long quizNum;
+    @NotNull(message = "카테고리를 선택하세요")
     private Long categoryNum; //숫자로 입력
+    @NotNull(message = "유저 정보가 입력되지 않았습니다")
     private Long userId; // 사용자 정보를 로그인 정보로 받아 오는 방법? or 직접 입력?
+    @NotNull(message = "퀴즈 점수를 입력하세요")
     private Integer quizScore;
 
     @NotNull(message = "문제 질문을 입력하세요")
